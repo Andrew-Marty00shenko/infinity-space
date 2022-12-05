3d22220150ecc421b692d502c57627a4ab277bdd
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-12-05 10:37:41 +0300

    Update FaqSection.jsx

--- src/components/Landing/FaqSection/FaqSection.jsx
+++ src/components/Landing/FaqSection/FaqSection.jsx
@@ -14,13 +14,14 @@ const FaqSection = () => {
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Where can I get a metamask wallet?</Accordion.Header>
                 <Accordion.Body>
-                    All actions and transactions are controlled by a smart contract. No one is able to influence its operation from outside. Consequently, it is completely secure.
+                    You can register a metamask wallet by using the metamask extension for the Google Chrome browser. You can download the extension here
+                    Or download the metamask app on your phone and register your wallet there.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>How many levels should I activate at the beginning?</Accordion.Header>
                 <Accordion.Body>
-                    All actions and transactions are controlled by a smart contract. No one is able to influence its operation from outside. Consequently, it is completely secure.
+                    You can activate any number of levels, but for the best start we recommend activating at least the first 3 levels.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
@@ -32,25 +33,25 @@ const FaqSection = () => {
             <Accordion.Item eventKey="3">
                 <Accordion.Header>Can my money disappear?</Accordion.Header>
                 <Accordion.Body>
-                    All actions and transactions are controlled by a smart contract. No one is able to influence its operation from outside. Consequently, it is completely secure.
+                    All transactions are allocated to participants' personal wallets. The balance of the smart contract itself never contains funds and is always zero. Therefore, your funds can disappear only if you give someone access to your wallet.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="4">
                 <Accordion.Header>КWhat is the role of the smart contract?</Accordion.Header>
                 <Accordion.Body>
-                    All actions and transactions are controlled by a smart contract. No one is able to influence its operation from outside. Consequently, it is completely secure.
+                    A smart contract coordinates and distributes transactions and activates your tiers. It enables all processes on the platform to happen automatically.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="5">
                 <Accordion.Header>How much can I earn?</Accordion.Header>
                 <Accordion.Body>
-                    All actions and transactions are controlled by a smart contract. No one is able to influence its operation from outside. Consequently, it is completely secure.
+                    Your earnings are not limited to anything. It depends on the number of completed cycles. And they, in turn, depend on how many new participants you and your partners have invited.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="6">
                 <Accordion.Header>How do I withdraw money?</Accordion.Header>
                 <Accordion.Body>
-                    All actions and transactions are controlled by a smart contract. No one is able to influence its operation from outside. Consequently, it is completely secure.
+                    All your rewards go straight to your personal Metamask wallet. Then you can exchange BUSD to any other currency through any cryptocurrency exchange.
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
