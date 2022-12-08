aa5cab64df17d87676f893753436a74a5dbafd3b
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-12-09 00:17:37 +0300

    Refactor

--- src/components/Landing/MainSection/MainSection.jsx
+++ src/components/Landing/MainSection/MainSection.jsx
@@ -2,28 +2,9 @@ import { Link } from "react-router-dom";
 
 import "./MainSection.scss";
 
-import Image1 from "../../../assets/images/main-section/image-1.png";
-import Image2 from "../../../assets/images/main-section/image-2.png";
-import Image3 from "../../../assets/images/main-section/image-3.svg";
-import Image4 from "../../../assets/images/main-section/image-4.svg";
-import Image5 from "../../../assets/images/main-section/image-5.png";
-import Image6 from "../../../assets/images/main-section/image-6.svg";
-import Image from "../../../assets/images/backgrounds/Image.png";
-
 const MainSection = ({ setClickedSignUp }) => {
     return <section className="main-section">
         <div className="main-section__content">
-            {/* <div className="main-section__back"></div>
-        <div className="images">
-            <img className="img-1" src={Image1} alt="" />
-            <div className="round"></div>
-            <img className="img-2" src={Image2} alt="" />
-            <img className="img-3" src={Image3} alt="" />
-            <img className="img-4" src={Image4} alt="" />
-            <img className="img-5" src={Image5} alt="" />
-            <img className="img-6" src={Image6} alt="" />
-        </div> */}
-
             <h1>
                 <span>
                     Smart defi solution
