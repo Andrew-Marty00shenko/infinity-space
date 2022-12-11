f67e321db54aad196d3306950b4a23b82024734c
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-12-11 23:58:08 +0300

    Update redirects

--- src/routes/Public.js
+++ src/routes/Public.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import Landing from "../pages/Landing/Landing";
 
 const Public = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!location.pathname.includes('home-page')) {
+            navigate('/')
+        }
+    }, []);
+
     return <Landing />
 }
 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,19 +1,10 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 import Public from "./Public";
 import Private from "./Private";
 
 const Routes = () => {
     const wallet = useSelector(state => state.user.wallet);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (wallet === null) {
-            navigate('/');
-        }
-    }, []);
 
     return wallet !== null ? <Private /> : <Public />
 }
